a49bf5a619ce9166bb7babe2632a6b4d05c8e4a2
Radhika Garg <r.garg@ufl.edu>
2012-01-18 10:15:11 -0600

    initial commit

---
+++ src/edu/ufl/cise/cop5555/sp12/Scanner.java
@@ -0,0 +1,25 @@
+package edu.ufl.cise.cop5555.sp12;
+
+
+import java.util.HashMap;
+
+import edu.ufl.cise.cop5555.sp12.TokenStream.Token;
+import static edu.ufl.cise.cop5555.sp12.Kind.*;
+
+
+public class Scanner {
+
+
+	public Scanner(TokenStream stream)  {
+		//TODO:  IMPLEMENT ME
+	}
+
+
+
+	public void scan() {
+		//TODO:  IMPLEMENT ME
+	}
+
+   //You will need to add fields and additional methods
+	
+}
